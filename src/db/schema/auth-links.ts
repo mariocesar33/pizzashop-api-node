28d9b7d23913023bbc4848517f925c526df93b94
mariocesar33 <mcgato33@hotmail.com>
2024-03-09 21:36:17 -0100

    Schema de Links de Autenticação

---
+++ src/db/schema/auth-links.ts
@@ -0,0 +1,14 @@
+import { createId } from '@paralleldrive/cuid2'
+import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+import { users } from '.'
+
+export const authLinks = pgTable('auth_links', {
+  id: text('id')
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  userId: text('user_id')
+    .references(() => users.id)
+    .notNull(),
+  code: text('code').notNull().unique(),
+  createdAt: timestamp('created_at').defaultNow(),
+})
