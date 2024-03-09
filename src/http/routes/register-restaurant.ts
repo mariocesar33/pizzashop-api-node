57fe62dc68318449baf7ac734cd19948df68779f
mariocesar33 <mcgato33@hotmail.com>
2024-03-09 16:22:59 -0100

    Tipagem na entrada de dados

--- src/http/routes/register-restaurant.ts
+++ src/http/routes/register-restaurant.ts
@@ -2,15 +2,25 @@ import { FastifyInstance } from 'fastify'
 
 import { db } from '../../db/connection'
 import { restaurants, users } from '../../db/schema'
+import { z } from 'zod'
 
 export async function registerRestaurant(app: FastifyInstance) {
   app.post('/restaurants', async (request, reply) => {
-    const { restaurantName, name, email, phone } = request.body as any
+    const registerRestaurantSchema = z.object({
+      restaurantName: z.string(),
+      managerName: z.string(),
+      email: z.string().email(),
+      phone: z.string(),
+      role: z.enum(['manager', 'customer']).default('manager'),
+    })
+
+    const { restaurantName, managerName, email, phone } =
+      registerRestaurantSchema.parse(request.body)
 
     const [manager] = await db
       .insert(users)
       .values({
-        name,
+        name: managerName,
         email,
         phone,
         role: 'manager',
